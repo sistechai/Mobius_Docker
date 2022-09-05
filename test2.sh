8aab771a387a1beccde08dca0718edc113d29f41
Shekhrozx <shekhrozx@gmail.com>
2022-09-05 16:26:20 +0900

    docker compose

--- test2.sh
+++ test2.sh
@@ -3,7 +3,7 @@
 
 echo -e "\n===========Retrieve <CSE> resource from Mobius docker============\n"
 curl -X GET \
-  'http://localhost:7579/Mobius?rcn=2' \
+  'http://192.168.0.138:7579/Mobius?rcn=1' \
   -H 'Accept: application/json' \
   -H 'X-M2M-Origin: SOrigin' \
   -H 'X-M2M-RI: 123';
