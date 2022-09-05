000c1cd5160726fc75855d07966e98ea29ba0f21
Shekhrozx <shekhrozx@gmail.com>
2022-09-05 17:06:26 +0900

    bash command

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo docker-compose start
-
-# >> $(date '+%Y-%m-%d-%T').log
-
+sudo docker-compose up -d
 
 echo -e "\n===========Result of Mobius docker run==============\n"
 sudo docker-compose ps
